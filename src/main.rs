0ba79fb2d9cc775b590cec2b8e86fa816fec1313
Dante Falzone <dantefalzone0@protonmail.com>
2022-03-31 16:04:48 +0000

    Added function for getting SHA-256 hash

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,26 @@
 #[macro_use] extern crate rocket;
+extern crate hex;
 use rocket::fs::FileServer;
+use sha2::{Sha256, Digest};
+
 
 #[get("/mudnix")]
 fn mudnix() -> &'static str {
   "Hello from Mudnix!"
 }
 
+#[get("/hash/<string_to_hash>")]
+fn hash(string_to_hash: &str) -> String {
+  let mut hasher = Sha256::new();
+  hasher.update(string_to_hash);
+  let hash = hasher.finalize();
+  hex::encode(hash)
+}
+
 #[launch]
 fn rocket() -> _ {
   rocket::build()
     .mount("/", routes![mudnix])
+    .mount("/sha256", routes![hash])
     .mount("/", FileServer::from("/home/runner/mudnix/static"))
 }
