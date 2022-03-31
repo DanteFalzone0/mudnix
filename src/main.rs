f2663e1f65fd421e973ca67d8794733ebec51d60
Dante Falzone <dantefalzone0@protonmail.com>
2022-03-31 05:21:17 +0000

    Set up web server

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,14 @@
-fn main() {
-    println!("Hello, world!");
+#[macro_use] extern crate rocket;
+use rocket::fs::FileServer;
+
+#[get("/mudnix")]
+fn mudnix() -> &'static str {
+  "Hello from Mudnix!"
+}
+
+#[launch]
+fn rocket() -> _ {
+  rocket::build()
+    .mount("/", routes![mudnix])
+    .mount("/", FileServer::from("/home/runner/mudnix/static"))
 }
