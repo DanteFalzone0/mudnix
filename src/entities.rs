bc7255df99458a4b93b07ac51cf0f011aac84d5c
Dante Falzone <dantefalzone0@protonmail.com>
2022-04-14 03:34:08 +0000

    implemented login function

--- src/entities.rs
+++ src/entities.rs
@@ -1,10 +1,12 @@
+use std::time::SystemTime;
 use serde::{Serialize, Deserialize};
 
-#[derive(Serialize, Deserialize)]
+#[derive(Serialize, Deserialize, Clone)]
 pub struct User {
   pub username: String,
   pub password_hash: String, // SHA-256 hash
-  pub world_location: String
+  pub world_location: String,
+  pub last_activity_timestamp: u64 // seconds since Unix epoch
 }
 
 impl User {
@@ -12,7 +14,9 @@ impl User {
     Self {
       username: username.to_string(),
       password_hash: password_hash.to_string(),
-      world_location: world_location.to_string()
+      world_location: world_location.to_string(),
+      last_activity_timestamp: SystemTime::now().duration_since(SystemTime::UNIX_EPOCH)
+        .unwrap().as_secs()
     }
   }
 }
@@ -25,5 +29,15 @@ pub struct UserList {
 impl UserList {
   pub fn contains(&self, username: &str) -> bool {
     self.users.iter().any(|user| user.username == username)
+  }
+
+  pub fn get_index_if_valid_creds(
+    &self,
+    username: &str,
+    password_hash: &str
+  ) -> Option<usize> {
+    self.users.iter().position(|user|
+      user.username == username && user.password_hash == password_hash 
+    )
   }
 }
--- src/main.rs
+++ src/main.rs
@@ -2,12 +2,14 @@
 extern crate hex;
 use std::fs;
 use std::sync::Mutex;
+use std::time::SystemTime;
 use rocket::fs::FileServer;
 use sha2::{Sha256, Digest};
 use rocket::http::Header;
 use rocket::{Request, Response};
 use rocket::fairing::{Fairing, Info, Kind};
 use rocket::State;
+use rocket::response::content;
 use serde_json;
 
 mod entities;
@@ -49,6 +51,10 @@ impl Fairing for CORS {
 
 struct UsersJsonFilePath {
   pub file_path_mutex: Mutex<String>
+}
+
+struct LoggedInUserPool {
+  pub users_mutex: Mutex<Vec<entities::User>>
 }
 
 #[get("/mudnix")]
@@ -95,15 +101,60 @@ fn new_user(
   }
 }
 
+#[get("/login?<username>&<password>")]
+fn login(
+  username: &str,
+  password: &str,
+  users_file_path_mutex: &State<UsersJsonFilePath>,
+  logged_in_user_pool: &State<LoggedInUserPool>
+) -> content::Json<String> {
+  let users_file_path: &str = &users_file_path_mutex.file_path_mutex
+    .lock().unwrap().to_string();
+  let original_json = fs::read_to_string(users_file_path).unwrap();
+  let mut user_list: entities::UserList = serde_json::from_str(&original_json)
+    .expect("unable to parse json from users.json");
+  let password_hash = hash(password);
+  if let Some(i) = user_list.get_index_if_valid_creds(username, &password_hash) {
+    let mut pool = logged_in_user_pool.users_mutex.lock().unwrap();
+    let already_logged_in = pool.iter().any(|user|
+      user.username == username
+    );
+    user_list.users[i].last_activity_timestamp = SystemTime::now()
+      .duration_since(SystemTime::UNIX_EPOCH).unwrap().as_secs();
+    if !already_logged_in {
+      pool.push(
+        user_list.users[i].clone()
+      );
+    }
+    let output_json = serde_json::to_string_pretty(&user_list).unwrap();
+    fs::write(users_file_path, output_json)
+      .expect("unable to save user to users.json");
+    content::Json(serde_json::json!({
+      "username": String::from(username),
+      "logged_in": true,
+      "was_previously_logged_in": already_logged_in
+    }).to_string())
+  } else {
+    content::Json(serde_json::json!({
+      "username": String::from(username),
+      "logged_in": false
+    }).to_string())
+  }
+}
+
 #[launch]
 fn rocket() -> _ {
   rocket::build()
     .manage(UsersJsonFilePath {
       file_path_mutex: Mutex::new(String::from("/home/runner/mudnix/users.json"))
     })
+    .manage(LoggedInUserPool {
+      users_mutex: Mutex::new(vec![])
+    })
     .attach(CORS)
     .mount("/", routes![mudnix])
     .mount("/hash", routes![hash])
     .mount("/user", routes![new_user])
+    .mount("/user", routes![login])
     .mount("/", FileServer::from("/home/runner/mudnix/static"))
 }
